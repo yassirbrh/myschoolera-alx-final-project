60d247cce7f3bf01f695222c796fef486c1b297f
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-03-25 18:37:58 +0000

    Creating the class model.

---
+++ models/classModel.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+
+const classSchema = new mongoose.Schema({
+    gradeLevel: {
+        type: String,
+        required: [true, "Please specify the grade level !!"]
+    },
+    gradeClass: {
+        type: Number,
+        required: [true, "Please specify the grade class !!"]
+    },
+    studentsList: {
+        type: [String],
+        required: true
+    },
+    examScores: {
+        type: mongoose.Schema.Types.Mixed,
+        required: false
+    }
+});
+
+const Class = mongoose.model('Class', classSchema);
+module.exports = Class;
