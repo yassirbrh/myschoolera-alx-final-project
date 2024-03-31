07eb0c624b51ff47c650fa946a75438fe3740a5a
Yassir Brhaiberh <yassir.brh46@gmail.com>
2024-03-31 19:01:42 +0100

    Updating server.js by defining client routes.

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cookieParser = require('cookie-parser');
 const dotenv = require('dotenv').config();
 const express = require('express');
 const errorHandler = require('./middlewares/errorMiddleware');
+const authAdminProtect = require('./middlewares/authAdminMiddleware');
 const mongoose = require('mongoose');
 const userRoute = require('./routes/userRoute');
 const studentRoute = require('./routes/studentRoute');
@@ -14,8 +15,14 @@ const app = express();
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: true,
+    credentials: true
+}));
 app.use(express.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/views'));
 
 app.use("/api/users", userRoute);
 app.use("/api/students", studentRoute);
@@ -23,8 +30,28 @@ app.use("/api/teachers", teacherRoute);
 app.use("/api/admin", adminRoute);
 
 
-app.get("/", (req, res) => {
-    res.send("Home Page !!\n");
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+app.get('/signup', (req, res) => {
+    res.sendFile(__dirname + '/views/signup.html');
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(__dirname + '/views/login.html');
+});
+
+app.get('/admin', (req, res) => {
+    res.sendFile(__dirname + '/views/admin.html');
+});
+
+app.get('/AdminDashboard', authAdminProtect, (req, res) => {
+    res.sendFile(__dirname + '/views/admin_dashboard.html');
+});
+
+app.get('/dashboard', (req, res) => {
+    res.sendFile(__dirname + '/views/dashboard/index.html');
 });
 
 // Error Handling Middleware
